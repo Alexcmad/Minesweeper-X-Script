22079dc7e9974ee21acd6c4357d05647f88223d5
Alexcmad <mcintoshalexander80@gmail.com>
2022-10-15 00:57:43 -0500

    Just take this version

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
 import pyautogui as pg
+import pygetwindow as pw
+import PIL
+
+window = pw.getWindowsWithTitle('Minesweeper X')[0]
+width = window.width
+height = window.height
+region = window.box
+
+columns = int((width - 30) / 16)
+rows = int((height - 116) / 16)
+
+game = []
+
+# all the different things that will appear
+# non-numbers:
+flag = "Buttons/flag.png"
+emptySpace = "Buttons/emptySpace.png"
+unchecked = "Buttons/unchecked.png"
+mine = "Buttons/mine.png"
+hitMine = "Buttons/hitMine.png"
+wrongFlag = "Buttons/wrongFlag.png"
+reset = "Buttons/reset.png"
+win = "Buttons/win.png"
+lose = "Buttons/lose.png"
+# numbers:
+one = "Buttons/one.png"
+two = "Buttons/two.png"
+three = "Buttons/three.png"
+four = "Buttons/four.png"
+five = "Buttons/five.png"
+six = "Buttons/six.png"
+seven = "Buttons/seven.png"
+eight = "Buttons/eight.png"
+
+
+def printGame():
+    for i in range (rows):
+        game.append([])
+        for j in range(columns):
+            game[i].append('')
+        print(game[i])
+    print(game)
+
 
 def main():
-    flag = pg.locateOnScreen('Buttons/flag.png')
-    unchecked = pg.locateCenterOnScreen('Buttons/lose.png')
-    print(unchecked)
+    for i in pg.locateAllOnScreen(unchecked, region=region):
+        print(pg.center(i))
+    printGame()
+
 
 if __name__ == '__main__':
     main()
