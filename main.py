661f947703716d9c40fe6415111534b23c590a8d
Alexcmad <113714949+Alexcmad@users.noreply.github.com>
2022-10-16 00:15:54 -0500

    IM LEARNING MORE

--- main.py
+++ main.py
@@ -18,22 +18,14 @@ rows = int((height - 116) / 16)
 print(f"Game Size:\nRows={rows}\nColums={columns}")
 
 game = []
-for i in range (rows):
+for i in range(rows):
     game.append([])
-    for j in range (columns):
+    for j in range(columns):
         game[i].append([''])
 
 for i in range(len(game)):
     for j in range(len(game[i])):
-        game[i][j]=('*')
-
-
-
-
-
-
-
-
+        game[i][j] = ('*')
 
 # all the different things that will appear
 # non-numbers:
@@ -56,37 +48,41 @@ six = "Buttons/six.png"
 seven = "Buttons/seven.png"
 eight = "Buttons/eight.png"
 
-
 try:
-    leftBorder = pg.locateOnScreen("Buttons/leftBorder.png",region)
-    topBorder = pg.locateOnScreen("Buttons/topBorder.png",region)
+    leftBorder = pg.locateOnScreen("Buttons/leftBorder.png", region)
+    topBorder = pg.locateOnScreen("Buttons/topBorder.png", region)
 except TypeError:
     print("Window not open. Try again with the window visible on the screen")
     window.activate()
     window.restore()
     quit()
 
-gridLeft = leftBorder[0]+leftBorder[2]
-gridTop = topBorder[1]+topBorder[3]
+gridLeft = leftBorder[0] + leftBorder[2]
+gridTop = topBorder[1] + topBorder[3]
 
-class cell:
-    def __init__(self, box):
-        self.box = box
-        self.w = box[2]
-        self.h = box[3]
-        self.left = box[0] - gridLeft
-        self.top = box[1] -gridTop
-        self.center = pg.center(box)
-        self.row = self.top//16
-        self.column = self.left//16
+
+def cell(box,type):
+        left = box[0] - gridLeft
+        top = box[1] - gridTop
+        center = pg.center(box)
+        row = top // 16
+        column = left // 16
+
+        d = {
+            "row":row,
+            "column":column,
+            "type":type
+        }
+
+        return d
 
 
 def main():
-    try:
-        c1 = cell(pg.locateOnScreen(unchecked,region))
-    except Exception:
-        print("Not Found")
-    print(f"Top: {c1.top} Left: {c1.left} Row: {c1.row} Column: {c1.column}")
+
+    flagList = [cell(i,'Flag') for i in pg.locateAllOnScreen(unchecked)]
+    print(flagList)
+    toprow = [i for i in flagList if i['row']==0]
+    print(toprow)
 
 
 if __name__ == '__main__':
