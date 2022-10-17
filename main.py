4b065a5c8540b819a0650fddad42f89195754e6b
Alexcmad <mcintoshalexander80@gmail.com>
2022-10-17 13:39:25 -0500

    Only the last algorithm to add and i kinda don't want to so im DONE

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def newGame():
 def scan():
     grid = []
     for x in btnList:
-        for i in pg.locateAllOnScreen(x, confidence=0.99, region=region):
+        for i in pg.locateAllOnScreen(x, confidence=0.99, region = region):
             grid.append(cell(i, x))
     return grid
 
@@ -138,7 +138,7 @@ def play(g):
 
             if len(uCount) + len(fCount) == v and uCount:
                 for x in uCount:
-                    if x['raw'] != flag:
+                    if x['raw']!=flag:
                         click(x["row"], x["column"], g, 'r')
                         x['raw'] = flag
                         x['type'] = 'F'
@@ -150,18 +150,23 @@ def play(g):
                     click(x["row"], x["column"], g, 'l')
                     notFound = False
 
+
     if notFound:
         unchecks = [x for x in g if x["raw"] == unchecked and x['notClicked']]
-        r = choice(unchecks)
-        click(r["row"], r["column"], g, 'l')
+        r = choice (unchecks)
+        click(r["row"],r["column"],g,'l')
+
+
 
     main()
+
+
 
 
 def main():
     game = sorted(scan(), key=lambda d: d['point'])
     for x in game:
-        if x['type'] == '*':
+        if x["type"] in ('*','!'):
             print("Game Over")
             newGame()
     play(game)
